bff3b9c1744b3cb297784c2e5fb020a2ae53f4c5
zxcvb31 <sergeevdanil2301@gmail.com>
2021-09-19 19:59:30 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$a = file_get_contents('http://task1/news.php');
+$link = 'http://task1/news.php';
+$StringB = mb_substr($a, 0,180);
+$WordsArray=explode(' ', $StringB);
+$PartWithoutLinks="";
+for($i=0;$i<count($WordsArray)-3;$i++) $PartWithoutLinks=$PartWithoutLinks.' '.$WordsArray[$i];
+//echo $c;
+$b = $PartWithoutLinks. '<a href="' . $link . '">' .' '. $WordsArray[count($WordsArray)-3] .' '.$WordsArray[count($WordsArray)-2] .' '.$WordsArray[count($WordsArray)-1]. '...</a>';
+echo $b;
+?>
