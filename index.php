412ff98822d3c4e89e83d00428b04180c884bdbb
zxcvb31 <sergeevdanil2301@gmail.com>
2021-09-19 20:02:50 +0300

    update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ $StringB = mb_substr($a, 0,180);
 $WordsArray=explode(' ', $StringB);
 $PartWithoutLinks="";
 for($i=0;$i<count($WordsArray)-3;$i++) $PartWithoutLinks=$PartWithoutLinks.' '.$WordsArray[$i];
-//echo $c;
 $b = $PartWithoutLinks. '<a href="' . $link . '">' .' '. $WordsArray[count($WordsArray)-3] .' '.$WordsArray[count($WordsArray)-2] .' '.$WordsArray[count($WordsArray)-1]. '...</a>';
 echo $b;
 ?>
